e00abcfd83db66fa8abbeccd575e1959fc87eee9
cho237 <kinslycho237@gmail.com>
2024-02-13 15:51:15 +0100

    adding scroll menu screen

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -39,159 +39,162 @@ class MenuScreen extends StatelessWidget {
               icon: const Icon(Icons.shopping_cart))
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 25),
-            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 40),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      'Get 32% Promo',
-                      style: GoogleFonts.dmSerifDisplay(
-                        fontSize: 20,
-                        color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        'Get 32% Promo',
+                        style: GoogleFonts.dmSerifDisplay(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    MyButton(
-                      text: "Reedeem",
-                      ontap: () {},
-                    ),
-                  ],
-                ),
-                Image.asset(
-                  'assets/sushi-roll.png',
-                  height: 80,
-                )
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Search here....",
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.white,
-                  ),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.white,
-                  ),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Text(
-              "Food Menu",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[800],
-                fontSize: 18,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: foodMenu.length,
-              itemBuilder: (context, index) => FoodTile(
-                food: foodMenu[index],
-                ontap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => FoodDetailsScreen(
-                        food: foodMenu[index],
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                  );
-                },
+                      MyButton(
+                        text: "Reedeem",
+                        ontap: () {},
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    'assets/sushi-roll.png',
+                    height: 80,
+                  )
+                ],
               ),
             ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.grey[100],
-              borderRadius: BorderRadius.circular(20),
+            const SizedBox(
+              height: 20,
             ),
-            margin: const EdgeInsets.only(
-              left: 25,
-              right: 25,
-              bottom: 25,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search here....",
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
             ),
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/futomaki.png',
-                      height: 60,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Salomon Eggss",
-                          style: GoogleFonts.dmSerifDisplay(
-                            fontSize: 18,
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Text(
+                "Food Menu",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[800],
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 220,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: foodMenu.length,
+                itemBuilder: (context, index) => FoodTile(
+                  food: foodMenu[index],
+                  ontap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => FoodDetailsScreen(
+                          food: foodMenu[index],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              margin: const EdgeInsets.only(
+                left: 25,
+                right: 25,
+                bottom: 25,
+              ),
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/futomaki.png',
+                        height: 60,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "Salomon Eggss",
+                            style: GoogleFonts.dmSerifDisplay(
+                              fontSize: 18,
+                            ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "\$21.00",
-                          style: TextStyle(color: Colors.grey[700]),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                const Icon(
-                  Icons.favorite_outline,
-                  color: Colors.grey,
-                  size: 28,
-                )
-              ],
-            ),
-          )
-        ],
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "\$21.00",
+                            style: TextStyle(color: Colors.grey[700]),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Icon(
+                    Icons.favorite_outline,
+                    color: Colors.grey,
+                    size: 28,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
